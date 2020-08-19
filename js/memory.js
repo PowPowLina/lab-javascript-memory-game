97af8ca74b3990049ef59d4add4528a82d238330
Diana <d.thueroff@gmail.com>
2020-08-19 23:03:38 +0200

    first iteration

--- js/memory.js
+++ js/memory.js
@@ -1,9 +1,24 @@
 class MemoryGame {
-  constructor(cards){
+  constructor(cards) {
     this.cards = cards;
     // add the rest of the class properties here
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
+
   }
-  shuffleCards() {}
-  checkIfPair(card1, card2) {}
+  shuffleCards() {
+    for (let i = this.cards.length - 1; i > 0; i--) {
+      let j = Math.floor(Math.random() * i);
+      let temp = this.cards[i];
+      this.cards[i] = this.cards[j];
+      this.cards[j] = temp;
+    }
+  }
+
+ checkIfPair(card1, card2) {
+   
+  }
+
   isFinished() {}
-}
+} 
